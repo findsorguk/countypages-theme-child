96ca34d0716986d44b13d94cbc7ccf13d0656551
Mary Chester-Kadwell <mchesterkadwell@gmail.com>
2016-10-27 11:15:35 +0100

    Change 'last updated' post meta to render by small template

---
+++ content-meta.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Adds date and updated information for the post
+ *
+ * @package countypages
+ */
+?>
+
+<div class="entry-meta">
+    <?php countypages_child_posted_on(); ?>
+</div><!-- .entry-meta -->
